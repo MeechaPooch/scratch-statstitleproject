5baf034d0c43b18d566219740a7f7bfcbb11d64a
micahpowch <powchm@catlin.edu>
2023-07-06 02:27:21 -0700

    use more up to date api wrapper

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 ///// SET YOUR USERNAME, PASSWORD, AND PROJECT ID HERE /////////
 const info = {
     username: "ilhp10",
-    password: "its a secret!!!",
+    password: "its a secret!",
     projectId: "509531164"
 }
 //////////////////////////////////////////////////////////////
 
 
 import Scratch from "scratch-api"
+import CloudApi from 'scratchcloudapi'
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -15,8 +16,8 @@ function sleep(ms) {
 
 function commafy(numStr) {
     numStr = new String(numStr)
-    for(let i=numStr.length-3; i>0; i-=3) {
-        numStr = numStr.slice(0,i) + ',' + numStr.slice(i,numStr.length)
+    for (let i = numStr.length - 3; i > 0; i -= 3) {
+        numStr = numStr.slice(0, i) + ',' + numStr.slice(i, numStr.length)
     }
     return numStr
 }
@@ -25,76 +26,46 @@ let lastProjectStats = {}
 
 import fetch from "node-fetch"
 
+let sess = new CloudApi.CloudSession(info.username, info.password, info.projectId)
 
-async function start() {Scratch.UserSession.create(info.username, info.password, async (err, user) => {
-    let token;
-    if (err) {
-         console.log(err)
-    } else {
+sess.on("connected", async () => {
+    // cloud.set("Variable Name", "Variable Value");
 
-        // get session token!
-        let sessionJson = await (await fetch("https://scratch.mit.edu/session/", {
+    let token = sess.user.token;
+    while (true) {
+        await sleep(1000)
+        // try {
+
+        let projectStats = (await (await fetch(`https://api.scratch.mit.edu/projects/${info.projectId}`)).json()).stats;
+        // if(projectStats.loves === lastProjectStats.loves && projectStats.favorites === lastProjectStats.favorites && projectStats.remixes === lastProjectStats.remixes && projectStats.views === lastProjectStats.views) {continue;}
+        // else {lastProjectStats = projectStats}
+        // console.log(projectStats)
+
+        fetch(`https://api.scratch.mit.edu/projects/${info.projectId}`, {
             "headers": {
-                "accept": "*/*",
+                "accept": "application/json",
                 "accept-language": "en, en;q=0.8",
+                "content-type": "application/json",
                 "sec-fetch-dest": "empty",
                 "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "x-requested-with": "XMLHttpRequest",
-                "cookie": `scratchlanguage=en; scratchsessionsid=${user.sessionId}; scratchcsrftoken=X8VBEl8rfW8qE5QcAanYOXXPXmlC00uH; permissions=%7B%22admin%22%3Afalse%2C%22scratcher%22%3Atrue%2C%22new_scratcher%22%3Afalse%2C%22social%22%3Atrue%2C%22educator%22%3Afalse%2C%22educator_invitee%22%3Afalse%2C%22student%22%3Afalse%2C%22mute_status%22%3A%7B%7D%7D`
+                "sec-fetch-site": "same-site",
+                "x-token": token
             },
             "referrer": "https://scratch.mit.edu/",
             "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
+            "body": `{\"title\":\"This project has ${commafy(projectStats.views)} VIEWS\",\"instructions\":\"This project has:\\n${commafy(projectStats.views)} VIEWS, \\n${commafy(projectStats.loves)} LOVES,\\n${commafy(projectStats.favorites)} FAVORITES,\\n${commafy(projectStats.remixes)} REMIXES!\\nRead notes/credits to learn how it works!\"}`,
+            "method": "PUT",
             "mode": "cors"
-        })).json();
-        token = sessionJson.user.token;
-        // console.log(token)
-        user.cloudSession(info.projectId, async (err, sess) => {
-            while (true) {
-		        await sleep(1000)
-                try{
-
-                let projectStats = (await (await fetch(`https://api.scratch.mit.edu/projects/${info.projectId}`)).json()).stats;
-                // if(projectStats.loves === lastProjectStats.loves && projectStats.favorites === lastProjectStats.favorites && projectStats.remixes === lastProjectStats.remixes && projectStats.views === lastProjectStats.views) {continue;}
-                // else {lastProjectStats = projectStats}
-                 // console.log(projectStats)
-
-                fetch(`https://api.scratch.mit.edu/projects/${info.projectId}`, {
-                    "headers": {
-                        "accept": "application/json",
-                        "accept-language": "en, en;q=0.8",
-                        "content-type": "application/json",
-                        "sec-fetch-dest": "empty",
-                        "sec-fetch-mode": "cors",
-                        "sec-fetch-site": "same-site",
-                        "x-token": token
-                    },
-                    "referrer": "https://scratch.mit.edu/",
-                    "referrerPolicy": "strict-origin-when-cross-origin",
-                    "body": `{\"title\":\"This project has ${commafy(projectStats.views)} VIEWS\",\"instructions\":\"This project has:\\n${commafy(projectStats.views)} VIEWS, \\n${commafy(projectStats.loves)} LOVES,\\n${commafy(projectStats.favorites)} FAVORITES,\\n${commafy(projectStats.remixes)} REMIXES!\\nRead notes/credits to learn how it works!\"}`,
-                    "method": "PUT",
-                    "mode": "cors"
-                }).then(async res => {/*console.log(await res.json()) */});
+        }).then(async res => {/*console.log(await res.json()) */ });
 
 
-                sess.set("☁ loves", projectStats.loves);
-                sess.set("☁ faves", projectStats.favorites);
-                sess.set("☁ remixes", projectStats.remixes);
-                sess.set("☁ views", projectStats.views);
-                sess.set("☁ newScratcherTestVal", 1);
-                console.log('done')
-            } catch(err) {
-                console.log(err)
-                start();
-		        break;
-                // return;
-            }
-            }
-        })
+        sess.set("loves", projectStats.loves);
+        sess.set("faves", projectStats.favorites);
+        sess.set("remixes", projectStats.remixes);
+        sess.set("views", projectStats.views);
+        sess.set("newScratcherTestVal", 1);
+        console.log('done')
+
     }
-});
-}
 
-start();
+});
